8fb64c7e4032634cf332507a11ea2e6a496eaea2
BrettCarter <brettcartersud@gmail.com>
2015-11-30 21:24:20 -0700

    I added a SQLStatements class that we can use for all of our different SQL statements. I created a method in there that will return all of the current jewelry items as an observable collection. This will keep the business logic out of the UI.

---
+++ CS3280GroupProject/CS3280GroupProject/clsSQLStatements.cs
@@ -0,0 +1,44 @@
+﻿using Group2_3280_Invoice;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS3280GroupProject
+{
+    class clsSQLStatements
+    {
+        clsDataAccess db;
+
+        public ObservableCollection<clsItem> itemsCollection()
+        {
+            clsDataAccess db;
+            ObservableCollection<clsItem> col_Items;
+            db = new clsDataAccess();
+            string sSQL;    //Holds an SQL statement
+            int iRet = 0;   //Number of return values
+            DataSet ds = new DataSet(); //Holds the return values
+            clsItem items; //Used to load the return values into the combo box
+            sSQL = "SELECT ItemCode, ItemDesc, Cost " + "FROM ItemDesc";
+            col_Items = new ObservableCollection<clsItem>();
+
+            ds = db.ExecuteSQLStatement(sSQL, ref iRet);
+
+            //Creates Flight objects based on the data pulled from the query than adds the object to a list
+            for (int i = 0; i < iRet; i++)
+            {
+                items = new clsItem();
+
+                items.ItemCode = ds.Tables[0].Rows[i][0].ToString();
+                items.ItemDesc = ds.Tables[0].Rows[i]["ItemDesc"].ToString();
+                items.Cost = ds.Tables[0].Rows[i]["Cost"].ToString();
+
+                col_Items.Add(items);
+            }
+            return col_Items;
+        }
+    }
+}
--- CS3280GroupProject/CS3280GroupProject/frmMainMenu.xaml.cs
+++ CS3280GroupProject/CS3280GroupProject/frmMainMenu.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CS3280GroupProject;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
@@ -22,72 +23,18 @@ namespace Group2_3280_Invoice
         private frmNewInvoice newInvoiceWindow;
         private frmUpdate updateWindow;
         private frmSearch searchWindow;
-
-        clsDataAccess db;
-        ObservableCollection<clsItem> col_Items;
+        private clsSQLStatements SQLStatements;
 
         public MainWindow()
 		{
 			this.InitializeComponent();
-
-            // Insert code required on object creation below this point.
-
-            ////MAKE SURE TO INCLUDE THIS LINE OR THE APPLICATION WILL NOT CLOSE
-            ////BECAUSE THE WINDOWS ARE STILL IN MEMORY
-            //Application.Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
-
-            ////Initialize the database class
-            //db = new clsDataAccess();
-
-            //string sSQL;    //Holds an SQL statement
-            //int iRet = 0;   //Number of return values
-            //DataSet ds = new DataSet(); //Holds the return values
-            //clsItem item; //Used to load the return values into the combo box
-
-            ////Create the SQL statement to extract the passengers
-            //sSQL = "SELECT ItemCode, ItemDesc, Cost " +
-            //    "FROM ItemDesc";
-
-            //ds = db.ExecuteSQLStatement(sSQL, ref iRet);
-
-            ////Loop through the data and load it into the combo box
-            //for (int i = 0; i < iRet; i++)
-            //{
-            //    item = new clsItem();
-            //    item.ItemCode = ds.Tables[0].Rows[i][0].ToString();
-            //    item.ItemDesc = ds.Tables[0].Rows[i]["ItemDesc"].ToString();
-            //    item.Cost = ds.Tables[0].Rows[i]["Cost"].ToString();
-
-            //    selectItem.Items.Add(item);
-            //}
-
-
-
-
+            //MAKE SURE TO INCLUDE THIS LINE OR THE APPLICATION WILL NOT CLOSE
+            //BECAUSE THE WINDOWS ARE STILL IN MEMORY
             Application.Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
 
-            db = new clsDataAccess();
-            string sSQL;    //Holds an SQL statement
-            int iRet = 0;   //Number of return values
-            DataSet ds = new DataSet(); //Holds the return values
-            clsItem items; //Used to load the return values into the combo box
-            sSQL = "SELECT ItemCode, ItemDesc, Cost " + "FROM ItemDesc";
-            col_Items = new ObservableCollection<clsItem>();
+            SQLStatements = new clsSQLStatements();
 
-            ds = db.ExecuteSQLStatement(sSQL, ref iRet);
-
-            //Creates Flight objects based on the data pulled from the query than adds the object to a list
-            for (int i = 0; i < iRet; i++)
-            {
-                items = new clsItem();
-
-                items.ItemCode = ds.Tables[0].Rows[i][0].ToString();
-                items.ItemDesc = ds.Tables[0].Rows[i]["ItemDesc"].ToString();
-                items.Cost = ds.Tables[0].Rows[i]["Cost"].ToString();
-
-                col_Items.Add(items);
-            }
-            selectItem.ItemsSource = col_Items;
+            selectItem.ItemsSource = SQLStatements.itemsCollection();
 
         }
 
